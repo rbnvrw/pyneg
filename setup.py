0498b35b6c983a5f734ba73121cb4895ec58af6d
Ruben Verweij <rbnvrw@users.noreply.github.com>
2019-07-30 21:53:03 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pyneg',
+      version='0.1',
+      description='Convert film negatives to positive images using Python',
+      url='https://github.com/rbnvrw/pyneg',
+      author='Ruben Verweij',
+      author_email='rbnvrw@gmail.com',
+      license='MIT',
+      packages=['pyneg'])
